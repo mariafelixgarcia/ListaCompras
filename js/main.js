ef8f20aa96a7598d3328a9014b24dbc012fddd3b
mariafelixgarcia <mariafelixgarciatellez@gmail.com>
2022-06-02 17:23:27 -0500

    se agregó almacenamiento local con 3 variables globales en localStorage

--- js/main.js
+++ js/main.js
@@ -88,14 +88,17 @@ txtNombre.style.border="";
 document.getElementById("alertValidaciones").style.display="none";
   contador++;
   document.getElementById("contadorProductos").innerHTML=contador;
+  //                       "nombredelallave", 
+localStorage.setItem("contadorProductos",contador);
   //el *100 es para que corra los dos ultimos digitos del numero aleatiro
     let precio= (Math.floor( (Math.random() * 50)*100))/100;
    let cantidad=parseFloat(txtNumber.value);
  totalEnProductos += Math.ceil(cantidad<1)?Math.ceil(cantidad):parseInt(cantidad);
  document.getElementById("productosTotal").innerHTML=totalEnProductos;
-   costoTotal+=(precio*cantidad);
+ localStorage.setItem("productosTotal",totalEnProductos); 
+ costoTotal+=(precio*cantidad);
    total.innerHTML=`$ ${costoTotal.toFixed(2)}`;
-   
+  localStorage.setItem("precioTotal",costoTotal.toFixed(2)); 
     let tmp= `<tr>
     <th scope="row">${contador}</th>
     <td>${txtNombre.value}</td>
@@ -122,4 +125,24 @@ txtNombre.addEventListener("blur", (event)=>{
 txtNumber.addEventListener("blur", (event)=>{
   event.target.value=event.target.value.trim ();
     }
+);
+
+window.addEventListener("load", function(){
+if (localStorage.getItem("contadorProductos")!=null){
+contador=parseInt(localStorage.getItem("contadorProductos"));
+document.getElementById("contadorProductos").innerHTML=contador;
+}//if contador
+
+if (localStorage.getItem("productosTotal")!=null){
+  totalEnProductos=parseInt(localStorage.getItem("productosTotal"));
+  document.getElementById("productosTotal").innerHTML=totalEnProductos;
+  }//if productostotal
+
+  if (localStorage.getItem("precioTotal")!=null){
+    costoTotal=parseFloat(localStorage.getItem("precioTotal"));
+    total.innerHTML=costoTotal;
+    }//if preciototal
+
+
+ }
 );
